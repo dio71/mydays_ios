edb29ff2b92d792a963edda517d4747a3b23b35d
dio71 <dio71.kim@gmail.com>
2026-05-26 17:52:30 +0900

    5/26 회사 작업 마감

---
+++ MyDaysWidget/MyDaysWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  MyDaysWidgetBundle.swift
+//  MyDaysWidget
+//
+//  Created by DioMini on 5/26/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MyDaysWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        MyDaysWidget()
+        MyDaysWidgetControl()
+    }
+}
---
+++ MyDaysWidget/MyDaysWidgetControl.swift
@@ -0,0 +1,54 @@
+//
+//  MyDaysWidgetControl.swift
+//  MyDaysWidget
+//
+//  Created by DioMini on 5/26/26.
+//
+
+import AppIntents
+import SwiftUI
+import WidgetKit
+
+struct MyDaysWidgetControl: ControlWidget {
+    var body: some ControlWidgetConfiguration {
+        StaticControlConfiguration(
+            kind: "io.snapplay.MyDays.MyDaysWidget",
+            provider: Provider()
+        ) { value in
+            ControlWidgetToggle(
+                "Start Timer",
+                isOn: value,
+                action: StartTimerIntent()
+            ) { isRunning in
+                Label(isRunning ? "On" : "Off", systemImage: "timer")
+            }
+        }
+        .displayName("Timer")
+        .description("A an example control that runs a timer.")
+    }
+}
+
+extension MyDaysWidgetControl {
+    struct Provider: ControlValueProvider {
+        var previewValue: Bool {
+            false
+        }
+
+        func currentValue() async throws -> Bool {
+            let isRunning = true // Check if the timer is running
+            return isRunning
+        }
+    }
+}
+
+struct StartTimerIntent: SetValueIntent {
+    static let title: LocalizedStringResource = "Start a timer"
+
+    @Parameter(title: "Timer is running")
+    var value: Bool
+
+    func perform() async throws -> some IntentResult {
+        // Start / stop the timer based on `value`.
+        return .result()
+    }
+}
